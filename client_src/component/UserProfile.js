ee78bd70d4064b20fd6d9bc1a1336ed00e54fef2
roger <roger.frauca@gmail.com>
2016-11-30 00:13:49 +0100

    add to heroku

--- client_src/component/UserProfile.js
+++ client_src/component/UserProfile.js
@@ -3,24 +3,24 @@ import React from 'react'
 
 class UserAnonimous extends React.Component{
   render () {
-  return  {/* menu prile quick info*/}
-    <div className="profile">
+  return <div className="profile">
+   {/* menu prile quick info*/}
       <div className="profile_pic">
       </div>
       <div className="profile_info">
         <span>No login</span>
         <h2>No user</h2>
       </div>
-    </div>
     {/* /menu prile quick info*/}
+    </div>
   }
 }
 
 class UserProfileInfo extends React.Component{
   render () {
     let user=this.props.user.getBasicProfile();
-   return  {/* menu prile quick info*/}
-    <div className="profile">
+   return  <div className="profile">
+   {/* menu prile quick info*/}
       <div className="profile_pic">
         <img src={user.getImageUrl()} alt=""/>
       </div>
@@ -28,8 +28,8 @@ class UserProfileInfo extends React.Component{
         <span>Welcome</span>
         <h2>{user.getName()}</h2>
       </div>
+  {/* /menu prile quick info*/}
     </div>
-    {/* /menu prile quick info*/}
   }
 }
 
